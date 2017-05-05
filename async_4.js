72aca0aa4d6e71760b91d363a5e4e870c940a078
IORTREGA <iortega@nearbpo.com>
2017-05-05 11:28:55 -0500

    4 ejemplos

---
+++ async_4.js
@@ -0,0 +1,15 @@
+const fetch = require("node-fetch");
+
+const fetchGitHubUser = async (handle) => {
+    const url = `https://api.github.com/users/${handle}`;
+    const response = await fetch (url);
+    return await response.json();
+    
+}
+
+(async  () => {
+    const user = await fetchGitHubUser('mariusschulz');
+    console.log(user.name);
+    console.log(user.location);
+})();
+
